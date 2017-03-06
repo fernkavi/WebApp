798ea3cf0bee122c09b16a8b100d7d0b4e061104
feriya <sss>
2017-03-06 15:50:43 +0700

    test 1

---
+++ db_connect.inc.php
@@ -0,0 +1,14 @@
+<?php
+    /*define("DSN","mysql:dbname=workshop6_2;host=localhost");
+    define("DBUSER","iplezab");
+    define("DBPASS","iplezab");*/
+define("DSN","mysql:dbname=web;host=localhost");
+define("DBU","web");
+define("DBPWD","123456789");
+try{
+        $con=new PDO(DSN,DBU,DBPWD);
+    }catch (PDOException $e){
+        echo 'Connection failed: '.$e->getMessage();
+    }
+ sfefeefefe
+    ?>
